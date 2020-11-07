757676c69120260cbb9a0484777b94053b4182db
Tejus <tejusrevi@gmail.com>
2020-11-07 13:03:32 -0500

    Added HTML+JS+CSS for filtering

--- website/view/controller.py
+++ website/view/controller.py
@@ -1,11 +1,21 @@
-#Program by Tejus Revi
+#Program by Ilusha Rathnayake, Rehmat Raju, Tejus Revi
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import Context
+
 import pymongo
 
-
+'''Stores all movies in DB'''
 listOfMovies = []
+'''Stores details like all possible genres, languages etc needed for filtering'''
+metadata = {
+    'genres': [],
+    'languages': [],
+    'maxYear': 0,
+    'minYear': 0,
+    'maxRuntime': 0
+}
+
 PASSWORD = "team20"
 DATABASE_NAME = "testDB"
 myclient = pymongo.MongoClient("mongodb+srv://tejus:"+PASSWORD+"@cluster0.ymzum.gcp.mongodb.net/"+DATABASE_NAME+"?retryWrites=true&w=majority")
@@ -17,6 +27,20 @@ cursorOfMovies = mycol.find()
 
 for movie in cursorOfMovies:
     listOfMovies.append(movie)
+
+    for genre in movie["Genre"]:
+        if genre not in metadata.get('genres'):
+            metadata['genres'].append(genre)
+    for genre in movie["Language"]:
+        if genre not in metadata.get('languages'):
+            metadata['languages'].append(genre)
+    if movie["Year"] > metadata["maxYear"]:
+         metadata["maxYear"] = movie["Year"]
+    if movie["Year"] < metadata["minYear"] or metadata["minYear"] == 0:
+         metadata["minYear"] = movie["Year"]
+    if movie["Runtime"] > metadata["maxRuntime"]:
+         metadata["maxRuntime"] = movie["Runtime"]
+print(metadata)
 
 def getSimiliarMovies(genres, countries, imdbid):
     eligibleMovies = {}
@@ -43,11 +67,8 @@ def getSimiliarMovies(genres, countries, imdbid):
         returnList.append(id)
     return returnList
 
-
-
-
 def View(request):
-    context = {"listOfMovies":listOfMovies}
+    context = {"listOfMovies":listOfMovies, "metadata": metadata}
     response = render(request, 'index.html', context)
     return response
 
@@ -62,4 +83,10 @@ def navigate_to_movie(request):
 def view_database(request):
     context = {"listOfMovies":listOfMovies}
     response = render(request, 'database.html', context)
+    return response
+
+def filter_list(request):
+    newList = [movie for movie in listOfMovies if movie["Year"] > 1995]
+    context = {"listOfMovies":newList, "metadata": metadata}
+    response = render(request, 'index.html', context)
     return response
--- website/view/urls.py
+++ website/view/urls.py
@@ -3,6 +3,7 @@ from . import controller
 
 urlpatterns = [
     path('', controller.View, name = 'home-page'),
+    path('filter_list',controller.filter_list,name="filter_list"),
     path('movie', controller.navigate_to_movie, name = 'navigate_to_movie'),
     path('view_database', controller.view_database, name = 'view_database'),
 ]
